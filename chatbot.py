f2d8214129d3a5eb32e27fd902bd61a6cff086aa
saanvichawla1903 <118972161+saanvichawla1903@users.noreply.github.com>
2024-03-08 13:15:36 -0800

    Update chatbot.py
    chatbot name is now chatty botter

--- chatbot.py
+++ chatbot.py
@@ -22,7 +22,7 @@ class Chatbot:
     def __init__(self, llm_enabled=False):
         # The chatbot's default name is `moviebot`.
         # TODO: Give your chatbot a new name.
-        self.name = 'moviebot'
+        self.name = 'Chatty Botter'
 
         self.llm_enabled = llm_enabled
 
